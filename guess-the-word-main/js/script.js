d94ec02fd9d77b0b59770a757a37d4da105df05c
tiff3b <tiffany3berger@gmail.com>
2025-06-22 12:33:03 -0400

    Display guessed letters and if player wins

--- guess-the-word-main/js/script.js
+++ guess-the-word-main/js/script.js
@@ -68,12 +68,55 @@ const playerGuess = function (input) {
 
 //capture the input
 const makeGuess = function (guess) {
-    guess = guess.toUpperCase();
+    guess= guess.toUpperCase();
     //display if player guessed letter already
     if (guessedLetters.includes(guess)) {
         message.innerText = "You already guessed this letter, try again.";
     } else {
         guessedLetters.push(guess);
         console.log(guessedLetters);
+        //call so letter displays if it hasn't been guessed
+        wrongGuess();
+        //call so letter displays if correct guess
+        wordInProgress(guessedLetters);
+    }
+};
+
+//show guessed letters on screen
+const wrongGuess = function (){
+    //empty ul
+    guessedLettersList.innerHTML = "";
+    //create and add new li
+    for (const letter of guessedLetters){
+        const li = document.createElement("li");
+    li.innerText = letter;
+    guessedLettersList.append(li);
+    }
+};
+
+//show word in progress
+const wordInProgress = function (guessedLetters) {
+    const wordUpper = word.toUpperCase();
+    //split word string into an array
+    const wordArray = wordUpper.split("");
+    const revealWord = [];
+    //check if word array contains letters from the guessed letters
+    for (const letter of wordArray) {
+        if (guessedLetters.includes(letter)){
+            revealWord.push(letter.toUpperCase());
+        } else {
+            revealWord.push("⚫");
+        }
+    }
+    //update the empty array where the word in progress is
+        wordProgress.innerText = revealWord.join("");  
+        playerWin();  
+};
+
+//player wins
+const playerWin = function (){
+    if (word.toUpperCase() === wordProgress.innerText) {
+        message.classList.add("win");
+        message.innerHTML = `<p class="highlight">You guessed the correct word! Congrats!</p>`;
     }
 };
